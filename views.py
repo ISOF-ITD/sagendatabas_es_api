f83dfa552c0ecd44a81090639a1379dc57e0d351
Rico Simke <rico.simke@isof.se>
2024-03-01 09:45:00 +0100

    Refactor metadata query to use "should" instead of "must"

--- views.py
+++ views.py
@@ -259,7 +259,9 @@ def createQuery(request, data_restriction=None):
 
 	# Hämtar documenter som har speciell typ av metadata. Exempel: `has_metadata=sitevision_url` (hämtar kurerade postar för matkartan).
 	if ('has_metadata' in request.GET):
-		query['bool']['must'].append({
+		# we use should, so that one of the conditions
+		# must be met, but not all of them
+		query['bool']['should'].append({
 			'match': {
 				'metadata.type': request.GET['has_metadata']
 			}
